0aff207e7b22b96431d582601fcf8573e4dbb51a
Sena <andy.cha0514@gmail.com>
2025-01-11 22:20:01 +0900

    修正2

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,29 +1,56 @@
+// import { notFound } from "next/navigation";
+// import { getNewsDetail } from "@/app/_libs/microcms";
+// import Article from "@/app/_components/Article";
+// import ButtonLink from "@/app/_components/ButtonLink";
+// import styles from "./page.module.css";
+
+// // type Props = {
+// //   params: {
+// //     slug: string;
+// //   };
+// //   searchParams:{
+// //     dk?:string;
+// //   }
+// // };
+
+// // export default async function Page({ params,searchParams }: Props) {
+// //   const data = await getNewsDetail(params.slug,{draftKey:searchParams.dk}).catch(notFound);
+// type Props = {
+//   params: Promise<{ slug: string }>;
+//   searchParams: { dk?: string };
+// };
+// export default async function Page({ params, searchParams }: Props) {
+//   const { slug } = await params;
+//   const data = await getNewsDetail(slug, { draftKey: searchParams.dk }).catch(
+//     notFound
+//   );
+
+//   return (
+//     <>
+//       <Article data={data} />
+//       <div className={styles.footer}>
+//         <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
+//       </div>
+//     </>
+//   );
+// }
+
+
 import { notFound } from "next/navigation";
 import { getNewsDetail } from "@/app/_libs/microcms";
 import Article from "@/app/_components/Article";
 import ButtonLink from "@/app/_components/ButtonLink";
 import styles from "./page.module.css";
 
-// type Props = {
-//   params: {
-//     slug: string;
-//   };
-//   searchParams:{
-//     dk?:string;
-//   }
-// };
-
-// export default async function Page({ params,searchParams }: Props) {
-//   const data = await getNewsDetail(params.slug,{draftKey:searchParams.dk}).catch(notFound);
 type Props = {
   params: Promise<{ slug: string }>;
-  searchParams: { dk?: string };
+  searchParams: Promise<{ dk?: string }>;
 };
+
 export default async function Page({ params, searchParams }: Props) {
   const { slug } = await params;
-  const data = await getNewsDetail(slug, { draftKey: searchParams.dk }).catch(
-    notFound
-  );
+  const { dk } = await searchParams;
+  const data = await getNewsDetail(slug, { draftKey: dk }).catch(notFound);
 
   return (
     <>
