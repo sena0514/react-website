85e0729214e721acb2c6e1ac985348411ec4a36d
Sena <andy.cha0514@gmail.com>
2025-01-11 22:29:47 +0900

    修正4

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -1,3 +1,36 @@
+// import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
+// import { notFound } from "next/navigation";
+// import NewsList from "@/app/_components/NewsList";
+// import Pagination from "@/app/_components/Pagination";
+// import Category from "@/app/_components/Category";
+// import { NEWS_LIST_LIMIT } from "@/app/_constants";
+
+// type Props = {
+//   params: {
+//     id: string;
+//   };
+// };
+
+// export default async function Page({ params }: Props) {
+//   const category = await getCategoryDetail(params.id).catch(notFound);
+
+//   const { contents: news,totalCount } = await getNewsList({
+//     limit: NEWS_LIST_LIMIT,
+//     filters: `category[equals]${category.id}`,
+//   });
+
+//   return (
+//     <>
+//       <p>
+//         <Category category={category} />
+//         　の一覧
+//       </p>
+//       <NewsList news={news} />
+//       <Pagination totalCount={totalCount} basePath={`/news/category/${category.id}`} />
+//     </>
+//   );
+// }
+
 import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import { notFound } from "next/navigation";
 import NewsList from "@/app/_components/NewsList";
@@ -6,15 +39,15 @@ import Category from "@/app/_components/Category";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 type Props = {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 };
 
 export default async function Page({ params }: Props) {
-  const category = await getCategoryDetail(params.id).catch(notFound);
+  const { id } = await params;
 
-  const { contents: news,totalCount } = await getNewsList({
+  const category = await getCategoryDetail(id).catch(notFound);
+
+  const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
     filters: `category[equals]${category.id}`,
   });
@@ -22,8 +55,7 @@ export default async function Page({ params }: Props) {
   return (
     <>
       <p>
-        <Category category={category} />
-        　の一覧
+        <Category category={category} /> の一覧
       </p>
       <NewsList news={news} />
       <Pagination totalCount={totalCount} basePath={`/news/category/${category.id}`} />
