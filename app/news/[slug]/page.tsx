9963775961cbf5c3e23c5e956eb7fef2f8f13b24
Sena <andy.cha0514@gmail.com>
2025-01-13 23:43:27 +0900

    ニュースにSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -47,6 +47,8 @@ type Props = {
   searchParams: Promise<{ dk?: string }>;
 };
 
+export const revalidate = 0;
+
 export default async function Page({ params, searchParams }: Props) {
   const { slug } = await params;
   const { dk } = await searchParams;
